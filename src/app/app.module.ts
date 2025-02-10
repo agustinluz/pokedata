4b85903af0c69b408f3f7f97eb6fea730fe8064f
agustinluz <agustinluzmartinll@gmail.com>
2025-02-10 11:51:09 +0100

    Añadir funcionalidad para mostrar y buscar Pokémon en la página de inicio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { RouteReuseStrategy } from '@angular/router';
-
-import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
-
+import { IonicModule } from '@ionic/angular';
 import { AppComponent } from './app.component';
-import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
+import { HomePage } from './home/home.page';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  declarations: [AppComponent, HomePage],
+  imports: [BrowserModule, IonicModule.forRoot(), HttpClientModule],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,13 +1,74 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PokemonService } from '../services/pokemon.service';
+import { Router } from '@angular/router';
+import { Pokemon } from '../modelo/pokemon.modelo';
+
 
 @Component({
   selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  templateUrl: './home.page.html',
+  styleUrls: ['./home.page.scss'],
   standalone: false,
 })
-export class HomePage {
+export class HomePage implements OnInit {
+  pokemonList: Pokemon[] = [];
+  limit = 20;
+  offset = 0;
+  searchTerm: string = '';
 
-  constructor() {}
+  constructor(private pokemonService: PokemonService, private router: Router) {}
 
+  ngOnInit() {
+    this.loadPokemon();
+  }
+
+  loadPokemon() {
+    this.pokemonService.getPokemonList(this.limit, this.offset).subscribe(response => {
+      this.pokemonList = response.results.map((pokemon: any) => {
+        const id = pokemon.url.split('/')[6];
+        return {
+          name: pokemon.name,
+          image: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`,
+          id: id
+        };
+      });
+    });
+  }
+
+  searchPokemon(event: any) {
+    const searchTerm = event.target.value.toLowerCase().trim();
+    if (!searchTerm) {
+      this.loadPokemon();
+      return;
+    }
+
+    this.pokemonService.getPokemonDetails(searchTerm).subscribe(
+      (response) => {
+        this.pokemonList = [{
+          name: response.name,
+          image: response.sprites.front_default,
+          id: response.id
+        }];
+      },
+      (error) => {
+        this.pokemonList = [];
+      }
+    );
+  }
+
+  nextPage() {
+    this.offset += this.limit;
+    this.loadPokemon();
+  }
+
+  prevPage() {
+    if (this.offset >= this.limit) {
+      this.offset -= this.limit;
+      this.loadPokemon();
+    }
+  }
+
+  viewDetails(name: string) {
+    this.router.navigate(['/pokemon-details', name]);
+  }
 }
